69e992349eb56ab6fe8ee2638266cfc5992f0bc9
Leslie Amegbor <lesliekofiameg@gmail.com>
2024-03-05 00:42:28 +0000

    Few modifications were made to logs.sh

--- assignment_008/1_bash/logs.sh
+++ assignment_008/1_bash/logs.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #The variable log_file is assigned the value of the path of the file logs.txt located in the same directory as the script.
 log_file="$(dirname "${BASH_SOURCE[0]}")/logs.txt"
 
@@ -8,7 +9,7 @@ if [[ ! -e $log_file ]]; then
 fi
 
 current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
-echo "Log entry at $current_datetime" >> $log_file
+echo "Log entry at $current_datetime" > $log_file
 echo "Log Entry added successfully"
 
 
